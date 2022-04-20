9c1d3402a75570a74140f63284af8c72c7a368a7
Yauheni Baradzko <yauheni.baradzko@itechart-group.com>
2022-04-20 11:53:16 +0200

    feat: add spinner

--- src/components/PreviewCardList/PreviewCardList.tsx
+++ src/components/PreviewCardList/PreviewCardList.tsx
@@ -3,20 +3,20 @@ import Container from '../Container/Container'
 import PreviewCardsItemProps from '../PreviewCardItem/PreviewCardsItem'
 import { useFetchAllPreviewCardsQuery } from '../../services/PreviewCardsService'
 import classes from './PreviewCardList.module.scss'
+import Spinner from '../Spinner/Spinner'
 
 const PreviewCardList: FC = () => {
   const { data: previewCards, error, isLoading } = useFetchAllPreviewCardsQuery()
 
   return (
     <Container>
-      {isLoading && <div>Loading...</div>}
+      {isLoading && <Spinner />}
       {error && <div>{error as ReactNode}</div>}
       <div className={classes.cardList}>
       {previewCards?.map((card) => (
         <PreviewCardsItemProps key={card.id} card={card} />
       ))}
       </div>
-      
     </Container>
   )
 }
---
+++ src/components/Spinner/Spinner.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react'
+import classes from './Spinner.module.scss'
+
+const Spinner: FC = () => {
+  return (
+    <div className={classes.spinner} />
+  )
+}
+
+export default Spinner
